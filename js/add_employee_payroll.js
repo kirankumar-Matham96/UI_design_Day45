62ec1c45908ce3ede14f16cea200470992ba4e5c
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-05-18 20:46:55 +0530

    ADD: JS files with local data storage

--- js/add_employee_payroll.js
+++ js/add_employee_payroll.js
@@ -1,147 +1,145 @@
 class EmployeePayrollData {
-  //properties
   id;
   name;
-  salary;
+  profilePic;
   gender;
+  department;
+  salary;
   startDate;
-  pinCode;
-  email;
+  note;
 
-  //constructor
-  constructor(...params) {
+  EmployeePayrollData(...params) {
     this.id = params[0];
     this.name = params[1];
-    this.salary = params[2];
+    this.profilePic = params[2];
     this.gender = params[3];
-    this.startDate = params[4];
+    this.department = params[4];
+    this.salary = params[5];
+    this.startDate = params[6];
+    this.note = params[7];
   }
 
-  /**
-   * getters and setters:
-   * while using setters and getters, we need to use '_' before every variable
-   */
-
-  //getters
-  getId() {
+  get id() {
     return this._id;
   }
 
-  getName() {
-    return this._name; //'-' used before variable 'name': must (syntax)
+  /**
+   * @param {any} id
+   */
+  set id(id) {
+    this._id = id;
   }
 
-  getSalary() {
-    return this._salary;
+  get name() {
+    return this._name;
   }
 
-  getGender() {
-    return this._gender;
-  }
-
-  getPinCode() {
-    return this._pinCode;
-  }
-
-  getEmail() {
-    return this._email;
-  }
-
-  //setters
-  setId(id) {
-    let idRegex = RegExp("^[1-9]{1}[0-9]{2,}$");
-    if (idRegex.test(id)) {
-      this.id = id;
-    } else {
-      throw "INCORRECT ID!";
-    }
-  }
-
-  setName(name) {
-    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$"); //not working
+  /**
+   * @param {string} name
+   */
+  set name(name) {
+    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
     if (nameRegex.test(name)) {
       this._name = name;
     } else {
-      throw "Incorrect name";
+      throw "name is invalid";
     }
   }
 
-  setSalary(salary) {
-    let salaryRegex = RegExp("^[1-9]{1}[0-9]{2,}$");
-    if (salaryRegex.test(salary)) {
-      this._salary = salary;
-    } else {
-      throw "INCORRECT SALARY!";
-    }
+  get profilePic() {
+    return this._profilePic;
   }
 
-  setGender(gender) {
-    let genderRegex = RegExp("(M|F)");
-    if (genderRegex.test(gender)) {
-      this._gender = gender;
-    } else {
-      throw "INCORRECT GENDER!";
-    }
+  /**
+   * @param {any} profilePic
+   */
+  set profilePic(profilePic) {
+    this._profilePic = profilePic;
   }
 
-  setStartDate(startDate) {
+  get gender() {
+    return this._gender;
+  }
+
+  /**
+   * @param {any} gender
+   */
+  set gender(gender) {
+    this._gender = gender;
+  }
+
+  get department() {
+    return this._department;
+  }
+
+  /**
+   * @param {any} department
+   */
+  set department(department) {
+    this._department = department;
+  }
+
+  get salary() {
+    return this._salary;
+  }
+
+  /**
+   * @param {any} salary
+   */
+  set salary(salary) {
+    this._salary = salary;
+  }
+
+  get startDate() {
+    return this._startDate;
+  }
+
+  /**
+   * @param {any} startDate
+   */
+  set startDate(startDate) {
+    //need to add date range condition
+    let currentDate = new Date();
+    let systemDay = currentDate.getDay();
+    let systemMonth = currentDate.getMonth();
+    let systemYear = currentDate.getFullYear();
+
     this._startDate = startDate;
   }
 
-  setPinCode(pinCode) {
-    let pinRegex = RegExp("^[1-9]{1}[0-9]{2,6}$");
-    if (pinRegex.test(pinCode)) {
-      this._pinCode = pinCode;
-    } else {
-      throw "INVALID PINCODE!";
-    }
+  get note() {
+    return this._note;
   }
 
-  setEmail(email) {
-    let emailRegex = RegExp(
-      "^(?!\\.)[A-Za-z0-9]+([._%+-]?[0-9])*@[A-Za-z0-9-]+\\.[a-zA-Z]{2,}(\\.[A-Za-z]{2,})?$"
-    );
-    if (emailRegex.test(email)) {
-      this._email = email;
-    } else {
-      throw "Invalid email!";
-    }
+  /**
+   * @param {any} note
+   */
+  set note(note) {
+    this._note = note;
   }
 
-  //toString
   toString() {
     const options = { year: "numeric", month: "long", day: "numeric" };
-    const empDate =
-      this.startDate === undefined
-        ? "undefined"
-        : this.startDate.toLocaleDateString("en-US", options); //see toLocaleDateString
+    const empDate = !this._startDate
+      ? "undefined"
+      : this._startDate.toLocaleDateString("en-US", options);
     return (
-      "id = " +
-      this.id +
-      ", name = " +
-      this.name +
-      ", gender = " +
-      this.gender +
-      ", salary = " +
-      this.salary +
-      ", Date started = " +
-      empDate
+      "id=" +
+      this._id +
+      ", name=" +
+      this._name +
+      ", gender=" +
+      this._gender +
+      ", profile pic=" +
+      this._profilePic +
+      ", department=" +
+      this._department +
+      ", salary=" +
+      this._salary +
+      ", start date=" +
+      empDate +
+      ", note=" +
+      this._note
     );
   }
 }
-
-let employeePayrollData = new EmployeePayrollData(1, "Me", 5000000); //inserting data
-console.log(employeePayrollData);
-employeePayrollData.setName("mememe"); //modifying name
-employeePayrollData.setid(0123); //throws incorrect id exception
-console.log(employeePayrollData);
-
-//new employee
-let employeePayrollData1 = new EmployeePayrollData(
-  2,
-  "Terisa",
-  3000000,
-  "F",
-  new Date()
-);
-console.log(employeePayrollData1);
---
+++ js/event_listener_for_payroll.js
@@ -0,0 +1,135 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  const name = document.querySelector("#name");
+  const nameError = document.querySelector(".name-error");
+  name.addEventListener("input", function () {
+    if (name.value.length === 0) {
+      nameError.textContent = "";
+      return;
+    } else {
+      try {
+        new EmployeePayrollData().name = name.value;
+        nameError.textContent = "";
+      } catch (e) {
+        nameError.textContent = e;
+      }
+    }
+  });
+
+  const salary = document.querySelector("#salary");
+  const salaryOutput = document.querySelector(".salary-output-text");
+  salaryOutput.textContent = salary.value;
+  salary.addEventListener("input", function () {
+    salaryOutput.textContent = salary.value;
+  });
+});
+
+const save = () => {
+  try {
+    let employeePayrollData = createEmployeePayroll();
+    createAndUpdateStorage(employeePayrollData);
+  } catch (e) {
+    return;
+  }
+};
+
+function createAndUpdateStorage(employeePayrollData) {
+  let employeePayrollList = JSON.parse(
+    localStorage.getItem("EmployeePayrollList")
+  );
+
+  if (employeePayrollList != undefined) {
+    employeePayrollList.push(employeePayrollData);
+  } else {
+    employeePayrollList = [employeePayrollData];
+  }
+  alert(employeePayrollList.toString());
+  localStorage.setItem(
+    "EmployeePayrollList",
+    JSON.stringify(employeePayrollList)
+  );
+}
+
+const createEmployeePayroll = () => {
+  let employeePayrollData = new EmployeePayrollData();
+  try {
+    employeePayrollData.name = getInputValueById("#name");
+  } catch (e) {
+    setTextValue(".text-error", e);
+    throw e;
+  }
+  employeePayrollData.profilePic = getSelectedValues("[name=profile]").pop();
+  employeePayrollData.gender = getSelectedValues("[name=gender]").pop();
+  employeePayrollData.department = getSelectedValues("[name=department]").pop();
+  employeePayrollData.salary = getInputValueById("#salary");
+  employeePayrollData.note = getInputValueById("#notes");
+  let date =
+    getInputValueById("#day") +
+    "" +
+    getInputValueById("#month") +
+    "" +
+    getInputValueById("#year");
+  employeePayrollData.date = Date.parse(date);
+  alert(employeePayrollData.toString());
+  return employeePayrollData;
+};
+
+/**
+ * gets the values of all the selected elements
+ */
+const getSelectedValues = (propertyValue) => {
+  let allItems = document.querySelectorAll(propertyValue);
+  let selItems = [];
+  allItems.forEach((item) => {
+    if (item.checked) selItems.push(item.value);
+  });
+  return selItems;
+};
+
+/**
+ * querySelector is the newer feature.
+ * It is used when selecting by element name,nesting or class-name.
+ * It will let you find elements with rules that can't be expressedwith getElementById.
+ */
+const getInputValueById = (id) => {
+  let value = document.querySelector(id).value;
+  return value;
+};
+
+/**
+ * getElementById is better supported than qurySelector method in older versions of the browsers.
+ * It will allow to select element by only it's id.
+ */
+const getInputElementValue = (id) => {
+  let value = document.getElementById(id).value;
+  return value;
+};
+
+const resetForm = () => {
+  setValue("#name", "");
+  unsetSelectedValues("[name=profile]");
+  unsetSelectedValues("[name=gender]");
+  unsetSelectedValues("[name=department]");
+  setValue("#salary", "");
+  setValue("#notes", "");
+  setValue("#day", "1");
+  setValue("#month", "Jan");
+  setValue("#year", "2016");
+  setTextValue("#salary-output", "400000");//not working!
+};
+
+const unsetSelectedValues = (propertyValue) => {
+  let allItems = document.querySelectorAll(propertyValue);
+  allItems.forEach((item) => {
+    item.checked = false;
+  });
+};
+
+const setTextValues = (id, value) => {//not used anywhere!
+  const element = document.querySelector(id);
+  element.textContenet = value;
+};
+
+const setValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.textContenet = value;
+};
